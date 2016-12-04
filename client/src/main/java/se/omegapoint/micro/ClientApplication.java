fb9c7adb78a505487af436c8ad7fa6dba518df9c
PNTS <pontus.thulin@omegapoint.se>
2016-12-04 16:57:16 +0100

    Fixes eureka clients to register with its public ip

--- client/src/main/java/se/omegapoint/micro/ClientApplication.java
+++ client/src/main/java/se/omegapoint/micro/ClientApplication.java
@@ -1,10 +1,14 @@
 package se.omegapoint.micro;
 
+import com.netflix.appinfo.AmazonInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.commons.util.InetUtilsProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -30,6 +34,24 @@ public class ClientApplication {
 			return Arrays.asList(restTemplate);
 
 		}
+
+		/**
+		 * Uses the public ip of the host machine when registering to Eureka.
+		 * This is required when running application with docker
+		 */
+		@Bean
+		public EurekaInstanceConfigBean eurekaInstanceConfig() {
+			EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(new InetUtils(new InetUtilsProperties()));
+			AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+			config.setDataCenterInfo(info);
+			info.getMetadata().put(AmazonInfo.MetaDataKey.publicHostname.getName(),
+					info.get(AmazonInfo.MetaDataKey.publicIpv4));
+			config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+			config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+			config.setNonSecurePort(8080);
+			return config;
+		}
+
 	}
 
 	public static void main(String[] args) {
